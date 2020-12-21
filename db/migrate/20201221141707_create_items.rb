c0043812db42e18ac012042ef3828aa67f8aa2f1
morizonotomokazu <75007433+morizonotomokazu@users.noreply.github.com>
2020-12-21 23:38:24 +0900

    itmeテーブル作成

--- db/migrate/20201221141707_create_items.rb
+++ db/migrate/20201221141707_create_items.rb
@@ -1,7 +1,9 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string        :items_neme     ,null: false
 
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_12_21_141707) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "items_neme", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
