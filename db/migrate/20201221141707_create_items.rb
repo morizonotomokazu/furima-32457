eda259780b5ea5bd53267f2109b4f5e6458a1065
morizonotomokazu <75007433+morizonotomokazu@users.noreply.github.com>
2020-12-26 15:57:22 +0900

    修正

--- db/migrate/20201221141707_create_items.rb
+++ db/migrate/20201221141707_create_items.rb
@@ -9,8 +9,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer     :status_id                 ,null: false
       t.integer     :shipping_fee_id           ,null: false
       t.integer     :prefecture_id             ,null: false
-      t.integer     :scheduled_delivery_id     ,null: false
-      t.references  :user                      , foreign_key: true
+      # t.integer     :scheduled_delivery_id     ,null: false
+      # t.references  :user                      , foreign_key: true
 
     
 
