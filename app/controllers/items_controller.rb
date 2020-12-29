cf18592840076531ebceea1a79b44b7ce81fb43d
morizonotomokazu <75007433+morizonotomokazu@users.noreply.github.com>
2020-12-29 15:40:59 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,10 @@
 class ItemsController < ApplicationController
-
-  def index
-
-  end
-
-
-  # before_action :move_to_index, except: [:index, :show]
   before_action :authenticate_user!,except: [:index]
   before_action :set_item, except: [:index, :new, :create]
 
-  
+  def index
+    @items = Item.all
+  end
 
   def new
      @item = Item.new
