2509cba7b53a2eee1cc0e5518a332a1c5da8231f
morizonotomokazu <75007433+morizonotomokazu@users.noreply.github.com>
2020-12-30 15:37:06 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index,:show]
-  before_action :set_item, only: [:show,:edit,:update]
-  before_action :prohibit_access,only:[:edit,:update]
+  before_action :set_item, only: [:show,:edit,:update,:destroy]
+  before_action :prohibit_access,only:[:edit,:update,:destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,13 +19,13 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  # def destroy
-    # if @item.destroy
-        #  redirect_to root_path
-    # else
-        #  render :show
-    # end
-  # end
+  def destroy
+    if @item.destroy
+         redirect_to root_path
+    else
+         render :show
+    end
+  end
 
   def edit
   end
